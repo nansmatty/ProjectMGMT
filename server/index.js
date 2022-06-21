a53d9c7038a6e7a575c2dbc86c0a36660a212dbc
nansmatty <narayanmaitysp1997@gmail.com>
2022-06-21 20:41:33 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema,
+		graphiql: process.env.NODE_ENV === 'development',
+	})
+);
+
+app.listen(
+	port,
+	console.log(`🔥🔥🔥🔥🔥 Server running on port ${port} 🔥🔥🔥🔥🔥`)
+);

4bf0a9b76bdacab7a64b19df90216afa5e4388fc
nansmatty <narayanmaitysp1997@gmail.com>
2022-06-23 02:29:09 +0530

    backend done

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -11,6 +11,23 @@ const {
 	GraphQLEnumType,
 } = require('graphql');
 
+//Project Type
+const ProjectType = new GraphQLObjectType({
+	name: 'Project',
+	fields: () => ({
+		id: { type: GraphQLID },
+		name: { type: GraphQLString },
+		description: { type: GraphQLString },
+		status: { type: GraphQLString },
+		client: {
+			type: ClientType,
+			resolve(parent, args) {
+				return Client.findById(parent.clientId);
+			},
+		},
+	}),
+});
+
 //Client Type
 const ClientType = new GraphQLObjectType({
 	name: 'Client',
@@ -22,39 +39,9 @@ const ClientType = new GraphQLObjectType({
 	}),
 });
 
-//Project Type
-const ProjectType = new GraphQLObjectType({
-	name: 'Project',
-	fields: () => ({
-		id: { type: GraphQLID },
-		client: {
-			type: ClientType,
-			resolve(parent, args) {
-				return Client.findById(args.id);
-			},
-		},
-		name: { type: GraphQLString },
-		description: { type: GraphQLString },
-		status: { type: GraphQLString },
-	}),
-});
-
 const RootQuery = new GraphQLObjectType({
 	name: 'RootQueryType',
 	fields: {
-		clients: {
-			type: new GraphQLList(ClientType),
-			resolve(parent, args) {
-				return Client.find();
-			},
-		},
-		client: {
-			type: ClientType,
-			args: { id: { type: GraphQLID } },
-			resolve(parent, args) {
-				return Client.findById(args.id);
-			},
-		},
 		projects: {
 			type: new GraphQLList(ProjectType),
 			resolve(parent, args) {
@@ -66,6 +53,19 @@ const RootQuery = new GraphQLObjectType({
 			args: { id: { type: GraphQLID } },
 			resolve(parent, args) {
 				return Project.findById(args.id);
+			},
+		},
+		clients: {
+			type: new GraphQLList(ClientType),
+			resolve(parent, args) {
+				return Client.find();
+			},
+		},
+		client: {
+			type: ClientType,
+			args: { id: { type: GraphQLID } },
+			resolve(parent, args) {
+				return Client.findById(args.id);
 			},
 		},
 	},
@@ -141,6 +141,39 @@ const mutation = new GraphQLObjectType({
 			},
 			resolve(parent, args) {
 				return Project.findByIdAndDelete(args.id);
+			},
+		},
+
+		//Update Project
+		updateProject: {
+			type: ProjectType,
+			args: {
+				id: { type: GraphQLNonNull(GraphQLID) },
+				name: { type: GraphQLString },
+				description: { type: GraphQLString },
+				status: {
+					type: new GraphQLEnumType({
+						name: 'ProjectStatusUpdate',
+						values: {
+							new: { value: 'Not Started' },
+							progress: { value: 'In Progress' },
+							completed: { value: 'Completed' },
+						},
+					}),
+				},
+			},
+			resolve(parent, args) {
+				return Project.findByIdAndUpdate(
+					args.id,
+					{
+						$set: {
+							name: args.name,
+							description: args.description,
+							status: args.status,
+						},
+					},
+					{ new: true }
+				);
 			},
 		},
 	},
